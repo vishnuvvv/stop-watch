386c464b2bf00020d3f726fb6c16bfe3bcc4e90a
VISHNU V <vishnusathyanathan@gmail.com>
2023-02-28 00:10:44 +0530

    modified

--- app.js
+++ app.js
@@ -13,23 +13,45 @@ let mins = 0;
 let secs = 0;
 
 startBtn.addEventListener("click", () => {
-
-    if(paused){
-        paused= false;
-        startTime = Date.now() - elapsedTime;
-        intervalId = setInterval(updateTime,75)
-    }
-})
-pauseBtn.addEventListener("click",()=>{})
-resetBtn.addEventListener("click",()=>{})
-
-function updateTime(){
+  if (paused) {
+    paused = false;
+    startTime = Date.now() - elapsedTime;
+    intervalId = setInterval(updateTime, 75);
+  }
+});
+pauseBtn.addEventListener("click", () => {
+  if (!paused) {
+    paused = true;
     elapsedTime = Date.now() - startTime;
+    clearInterval(intervalId);
+  }
+});
+resetBtn.addEventListener("click", () => {
+  paused = true;
+  clearInterval(intervalId);
+  startTime = 0;
+  elapsedTime = 0;
+  currentTime = 0;
+  hrs = 0;
+  mins = 0;
+  secs = 0;
+  timeDisplay.textContent = "00:00:00";
+});
 
-    secs =Math.floor((elapsedTime / 1000) % 60)
-    mins = Math.floor((elapsedTime / (1000*60)) % 60)
-    hrs = Math.floor((elapsedTime / (1000*60*60)) % 60)
+function updateTime() {
+  elapsedTime = Date.now() - startTime;
 
-    timeDisplay.textContent = `${hrs}:${mins}:${secs}`
+  secs = Math.floor((elapsedTime / 1000) % 60);
+  mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+  hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
 
+  secs = pad(secs);
+  mins = pad(mins);
+  hrs = pad(hrs);
+
+  timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
+  function pad(unit) {
+    return ("0" + unit).length > 2 ? unit : "0" + unit;
+  }
 }
